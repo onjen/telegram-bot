ef47ffd1a4addadc9313b5931838553756a7fe35
Johannes Rothe <johannes.rothe@rwth-aachen.de>
2017-08-20 15:49:18 +0200

    Nicer output and only answer weather msgs

--- run.py
+++ run.py
@@ -15,13 +15,13 @@ try:
 except KeyError:
     print('KeyError while reading config')
 
-telegram_url = 'https://api.telegram.org/' + bot_id + ':' + telegram_key + '/'
+telegram_url = 'https://api.telegram.org/bot' + bot_id + ':' + telegram_key + '/'
 weather_url = 'http://api.openweathermap.org/data/2.5/'
 
-def send_message(msg):
+def send_message(msg,parse_mode=None):
     r = requests.post(telegram_url + 'sendMessage', 
-            data=dict(chat_id=chat_id,text=msg))
-    print r.json()
+            data=dict(chat_id=chat_id,text=msg,parse_mode=parse_mode))
+    print(r.json())
 
 def send_picture(url):
     r = requests.post(telegram_url + 'sendPhoto', 
@@ -38,30 +38,41 @@ def get_messages(response):
 def get_chat_id(response):
     return response.get('result')[0].get('message').get('chat').get('id')
 
-def weather_request(location):
+def weather_request(location,mode=None):
     r = requests.post(weather_url + 'weather',
-            params=dict(q=location,appid=weather_key,units='metric',lang='de'))
-    if r.ok:
+            params=dict(q=location,appid=weather_key,units='metric',
+                mode=mode,lang='de'))
+    print(r.content)
+    if not mode:
         return r.json()
     else:
-        print('Request not successful with {}'.format(r.url))
-        return None
+        return r.content
 
 def format_weather(json_data):
     location = json_data['name'].encode('utf-8')
     temp = json_data['main']['temp']
     desc = json_data['weather'][0]['description'].encode('utf-8')
-    answer = 'Wetter in {}\nTemperatur: {} Grad\n{}'.format(location,
-            temp, desc)
+    clouds = json_data['clouds']['all']
+    humidity = json_data['main']['humidity']
+    wind = json_data['wind']['speed']
+    answer = ('<strong>{}</strong>\n' +
+              '{} Grad\n' +
+              '{}\n' +
+              '<i>Wolken: {}%</i>\n' +
+              '<i>Wind: {} m/s</i>\n' +
+              '<i>Feuchtigkeit: {}%</i>').format(location, temp, desc,
+                      clouds, wind, humidity)
     return answer
 
-def get_weather_pic_url(json_data):
+def get_weather_pic_url(location):
+    json_data = weather_request(location)
     name = json_data['weather'][0]['icon']
     url = 'http://openweathermap.org/img/w/' + name + '.png'
     return url
 
 def main():
     offset = 0 
+    in_answer = False
     while True:
         # we have new messages
         response = get_response(offset+1, 60)
@@ -76,10 +87,14 @@ def main():
             send_message('You said: {}'.format(answer))
         if answer.lower() == "wetter":
             send_message('An welchem Ort?')
+            in_answer = True
             continue
-        json_weather = weather_request(answer + ',de')
-        send_picture(get_weather_pic_url(json_weather))
-        send_message(format_weather(json_weather))
+        if in_answer:
+            location = answer + ',de'
+            json_weather = weather_request(location)
+            send_picture(get_weather_pic_url(location))
+            send_message(format_weather(json_weather),'HTML')
+            in_answer = False
 
 if __name__ == '__main__':
     try:
